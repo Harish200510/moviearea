760c19d2115ec445c88cd52938e1ddd74e476036
Harish200510 <harish.venkatesan2005@gmail.com>
2025-05-31 14:57:51 +0530

    route structure

--- main.js
+++ main.js
@@ -7,4 +7,28 @@ app.get("/",(req,res)=>{
     res.json({msg:"Hello World!"})
 })
 
+//CRUD fucntionality of movies
+
+//R- For Reading
+app.get('/movies',()=>{
+
+})
+
+//C- For Creating
+app.post('/movies',()=>{
+
+})
+
+//U- For Updating
+app.put('/movies/:id',()=>{
+
+})
+
+//D- For Deleting
+app.delete('movies/:id',()=>{
+
+})
+
+
+
 app.listen(PORT,()=>{console.log(`Server is running at http://localhost:${PORT}`)})
